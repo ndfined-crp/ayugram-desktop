d188546605a01e1e0cd388a7c9cc4e8dd317b8d7
kaeeraa <kaeeraa@nebula-nook.ru>
2024-08-31 11:33:28 +0300

    :alembic: Perform experiments.

--- flake.nix
+++ flake.nix
@@ -5,20 +5,24 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   nixConfig = {
-    extra-substituters = [
-      "https://kaeeraa.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "kaeeraa.cachix.org-1:S3CnhT12akYQf4Ph7fndLgqo2os4ket3OTP2amrzJRs="
-    ];
+    extra-substituters = [ "https://kaeeraa.cachix.org" ];
+    extra-trusted-public-keys = [ "kaeeraa.cachix.org-1:S3CnhT12akYQf4Ph7fndLgqo2os4ket3OTP2amrzJRs=" ];
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
       {
         packages = rec {
-          ayugram-desktop = (pkgs.libsForQt5.callPackage ./default.nix {});
+          ayugram-desktop = (pkgs.libsForQt5.callPackage ./default.nix { });
           default = ayugram-desktop;
         };
       }
