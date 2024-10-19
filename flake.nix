b8ead0e9d1731c208b5e4225be38a9a27191292f
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-10-19 09:53:06 +0000

    final flake.nix version

--- flake.nix
+++ flake.nix
@@ -5,17 +5,11 @@
         nixpkgs = {
             url = "github:nixos/nixpkgs/nixos-unstable";
         };
-
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
     };
 
     outputs = {
         self,
         nixpkgs,
-        home-manager,
         ...
     } @ inputs : 
     let
@@ -42,7 +36,5 @@
         extra-trusted-public-keys = [ 
             "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         ];
-
-        sandbox = true;
     };
 }
