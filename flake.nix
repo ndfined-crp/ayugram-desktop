87e8581ce89554a60724b942329e4e62f2841222
kaeeraa <kaeeraa@nebula-nook.ru>
2024-10-22 10:33:25 +0300

    flake: add missing bracket

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
+  };
   outputs =
     {
       self,
@@ -14,20 +15,17 @@
     let
       forAllSystems =
         function:
-          nixpkgs.lib.genAttrs [
-            "x86_64-linux"
-            "aarch64-linux"
-            "x86_64-darwin"
-            "aarch64-darwin"
-          ] ( system:  
-              function nixpkgs.legacyPackages.${system}
-            );
-    in {
-      packages = forAllSystems ( 
-        pkgs: {
-          ayugram-desktop = pkgs.libsForQt5.callPackage ./default.nix { };
-        }
-      );
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        ayugram-desktop = pkgs.libsForQt5.callPackage ./default.nix { };
+      });
     };
 
   nixConfig = {
