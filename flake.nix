1f48f8c5ef62d5f8869673c82726b509aeabf8c5
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-10-22 07:24:53 +0000

    edited: flake.nix

--- flake.nix
+++ flake.nix
@@ -5,51 +5,36 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
   outputs =
     {
       self,
       nixpkgs,
-      home-manager,
       ...
     }@inputs:
-
     let
       forAllSystems =
         function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ] (system: function nixpkgs.legacyPackages.${system});
-    in
-    {
-
-      nixosModules = {
-        default = self.nixosModules;
-      };
-
-      homeManagerModules = {
-        default = self.nixosModules;
-      };
-
-      packages = forAllSystems (pkgs: {
-        ayugram-desktop = pkgs.libsForQt5.callPackage ./default.nix { };
-      });
+          nixpkgs.lib.genAttrs [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ] ( system:  
+              function nixpkgs.legacyPackages.${system}
+            );
+    in {
+      packages = forAllSystems ( 
+        pkgs: {
+          ayugram-desktop = pkgs.libsForQt5.callPackage ./default.nix { };
+        }
+      );
     };
 
   nixConfig = {
+    sandbox = true;
     extra-substituters = [
       "https://cache.garnix.io"
     ];
-
     extra-trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
