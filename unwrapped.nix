6de49d7caf60e837887368184646b203c3d77f06
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-14 22:57:32 +0300

    fix(patch): fix syntax err
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- unwrapped.nix
+++ unwrapped.nix
@@ -55,7 +55,7 @@ stdenv.mkDerivation (finalAttrs: {
   patches = [
     ./patch/cstring.patch
     # Fixes linux builds
-    (fecthpatch {
+    (fetchpatch {
       url = "https://github.com/AyuGram/AyuGramDesktop/pull/32/commits/15287ad6ed162c209d9772fc592e959d793f63b9";
       hash = "sha256-a3Mf+GcyS+dYritbqRpd9sFDpw/cXRA2Uv06+gac5mU=";
     })
