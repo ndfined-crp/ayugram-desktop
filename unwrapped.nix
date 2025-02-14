f525ab15562f5a56df38ef882cb1ac467055acb6
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-14 22:59:04 +0300

    fix(patch): update url
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- unwrapped.nix
+++ unwrapped.nix
@@ -56,7 +56,7 @@ stdenv.mkDerivation (finalAttrs: {
     ./patch/cstring.patch
     # Fixes linux builds
     (fetchpatch {
-      url = "https://github.com/AyuGram/AyuGramDesktop/pull/32/commits/15287ad6ed162c209d9772fc592e959d793f63b9";
+      url = "https://github.com/AyuGram/AyuGramDesktop/pull/32/commits/15287ad6ed162c209d9772fc592e959d793f63b9.patch";
       hash = "sha256-a3Mf+GcyS+dYritbqRpd9sFDpw/cXRA2Uv06+gac5mU=";
     })
   ];
