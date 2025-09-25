d5b4642427cdc6ebfe6e8d9082e918e180c756de
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-09-25 21:14:01 +0300

    fix(gdk): make another one workaround for issue NixOS/nixpkgs#424868
    This commits disables DMABUF renderer for webkits
    
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   libjxl,
   wrapGAppsHook3,
   wrapQtAppsHook,
+  makeBinaryWrapper,
   glib-networking,
   webkitgtk_4_1,
   withWebkit ? true,
@@ -28,6 +29,7 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs =
     [
       wrapQtAppsHook
+      makeBinaryWrapper
     ]
     ++ lib.optionals withWebkit [
       wrapGAppsHook3
@@ -78,6 +80,7 @@ stdenv.mkDerivation (finalAttrs: {
       wrapQtApp "$out/Applications/${finalAttrs.meta.mainProgram}.app/Contents/MacOS/${finalAttrs.meta.mainProgram}"
     ''
     + lib.optionalString stdenv.hostPlatform.isLinux ''
+      wrapProgram $out/bin/AyuGram --set WEBKIT_DISABLE_DMABUF_RENDERER enable
       substituteInPlace $out/share/dbus-1/services/* \
         --replace-fail "$unwrapped" "$out"
     '';
