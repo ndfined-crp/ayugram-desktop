ebec893e7347cf8c5387652738ef6b3a6770409f
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-11-26 20:58:39 +0300

    feat: rewrite default
    Signed-off-by: Kaeeraa <kaeeraa@nebula-nook.ru>

--- default.nix
+++ default.nix
@@ -10,9 +10,6 @@
   qtsvg,
   qtwayland,
   kimageformats,
-  libavif,
-  libheif,
-  libjxl,
   wrapGAppsHook3,
   wrapQtAppsHook,
   glib-networking,
@@ -22,46 +19,25 @@
 stdenv.mkDerivation (finalAttrs: {
   inherit pname;
   inherit (finalAttrs.unwrapped) version meta passthru;
-
   inherit unwrapped;
 
   nativeBuildInputs =
-    [
-      wrapQtAppsHook
-    ]
-    ++ lib.optionals withWebkit [
-      wrapGAppsHook3
-    ];
-
+    [wrapQtAppsHook]
+    ++ lib.optionals withWebkit [wrapGAppsHook3];
   buildInputs =
     [
       qtbase
       qtimageformats
       qtsvg
-    ]
-    ++ lib.optionals stdenv.hostPlatform.isLinux [
       kimageformats
       qtwayland
     ]
-    ++ lib.optionals stdenv.hostPlatform.isDarwin [
-      libavif
-      libheif
-      libjxl
-    ]
-    ++ lib.optionals withWebkit [
-      glib-networking
-    ];
+    ++ lib.optionals withWebkit [glib-networking];
 
-  qtWrapperArgs = lib.optionals (stdenv.hostPlatform.isLinux && withWebkit) [
-    "--prefix"
-    "LD_LIBRARY_PATH"
-    ":"
-    (lib.makeLibraryPath [webkitgtk_4_1])
-  ];
+  qtWrapperArgs = lib.optionals withWebkit ["--prefix" "LD_LIBRARY_PATH" ":" (lib.makeLibraryPath [webkitgtk_4_1])];
 
   dontUnpack = true;
   dontWrapGApps = true;
-  dontWrapQtApps = stdenv.hostPlatform.isDarwin;
 
   installPhase = ''
     runHook preInstall
@@ -69,16 +45,11 @@ stdenv.mkDerivation (finalAttrs: {
     runHook postInstall
   '';
 
-  preFixup = lib.optionalString (stdenv.hostPlatform.isLinux && withWebkit) ''
+  preFixup = lib.optionalString withWebkit ''
     qtWrapperArgs+=("''${gappsWrapperArgs[@]}")
   '';
-
-  postFixup =
-    lib.optionalString stdenv.hostPlatform.isDarwin ''
-      wrapQtApp "$out/Applications/${finalAttrs.meta.mainProgram}.app/Contents/MacOS/${finalAttrs.meta.mainProgram}"
-    ''
-    + lib.optionalString stdenv.hostPlatform.isLinux ''
-      substituteInPlace $out/share/dbus-1/services/* \
-        --replace-fail "$unwrapped" "$out"
-    '';
+  postFixup = ''
+    substituteInPlace $out/share/dbus-1/services/* \
+      --replace-fail "$unwrapped" "$out"
+  '';
 })
