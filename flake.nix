f851600fe115c5f069ffd433f0081602173ded4b
kaeeraa <kaeeraa@nebula-nook.ru>
2026-07-27 00:54:04 +0300

    144 garnix is no more (#148)
    closes #144
    
    ---------
    
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- flake.nix
+++ flake.nix
@@ -11,12 +11,10 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.garnix.io"
       "https://ayugram-desktop.cachix.org"
       "https://tg-owt.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "ayugram-desktop.cachix.org-1:AZ5EqHrJsAKL5YkZYLPEsb1FdD9QlypUwQ0REcJftgA="
       "tg-owt.cachix.org-1:lp0BukIhSK3EIyLcDhDZ5zABgT48nmNp6t4SnZ0wr8w="
     ];
